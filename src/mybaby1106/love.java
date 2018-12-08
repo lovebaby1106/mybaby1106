93a9e9d0051987b4b6d8b9662f5a0d7fdca09c37
eddie <a97390224@gmail.com>
2018-12-08 21:43:56 +0800

    我的愛只能給一個人!!!

--- src/mybaby1106/love.java
+++ src/mybaby1106/love.java
@@ -5,7 +5,7 @@ public class love {
 	public static void main(String[] args) {
 		System.out.println("my girl is very beautiful!!");
 		System.out.println("你以為小牛只有一個嗎? 等有小牛牛你就會說別的了!!!");
-
+		System.out.println("不行，我只愛一個牛");
 	}
 
 }
