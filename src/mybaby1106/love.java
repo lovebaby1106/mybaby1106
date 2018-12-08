e8b94a5912a42df5b5ce5f4d3544d80df0368900
alcawei <alcawei@gmail.com>
2018-12-08 21:49:16 +0800

    :我不許你們這麼說:

--- src/mybaby1106/love.java
+++ src/mybaby1106/love.java
@@ -6,6 +6,7 @@ public class love {
 		System.out.println("my girl is very beautiful!!");
 		System.out.println("你以為小牛只有一個嗎? 等有小牛牛你就會說別的了!!!");
 		System.out.println("不行，我只愛一個牛");
+		System.out.println("我不許你們這麼說");
 	}
 
 }
