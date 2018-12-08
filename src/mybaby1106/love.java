abfc0ff933d126f096c2c1d851c112b1535ffddc
eddie <a97390224@gmail.com>
2018-12-08 21:46:56 +0800

    神奇小牛

--- src/mybaby1106/love.java
+++ src/mybaby1106/love.java
@@ -7,6 +7,7 @@ public class love {
 		System.out.println("你以為小牛只有一個嗎? 等有小牛牛你就會說別的了!!!");
 		System.out.println("不行，我只愛一個牛");
 		System.out.println("是喝醉酒的牛還是雞與牛的牛?");
+		System.out.println("是德克斯特的實驗室");
 	}
 
 }
